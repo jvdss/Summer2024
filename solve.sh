daec9e84e244a48f8bea1bc112cbeb70d0946f7d
João Santos <87877655+jvdss@users.noreply.github.com>
2024-01-21 18:38:27 -0300

    Update solve.sh

--- solve.sh
+++ solve.sh
@@ -4,6 +4,5 @@ g++ -fsanitize=address,undefined -fno-omit-frame-pointer -g \
 -o a.out -Wall -Wshadow -std=c++17 -Wno-unused-result \
 -Wno-sign-compare -Wno-char-subscripts \
 "$1".cpp -o a.out && \
-./a.out < input.in > output.out  && \
-cat output.out
+./a.out < input.in > output.out 
 
